8cbeb222d1596c974601107bc126c8e6b9f945ea
Clement-Szewczyk <clement.szewczyk@gmail.com>
2022-03-07 12:24:05 +0100

    V1

--- HEI/python/morpion.py
+++ HEI/python/morpion.py
@@ -24,32 +24,67 @@ def calculerscore(t):
     if t[0][0] == t[1][1] and t[1][1] == t[2][2]:
         if t[0][0]=="X":
             return 10
-        elif t[row][0]=="O":
+        elif t[0][0]=="O":
             return -10
     if t[0][2] == t[1][1] and t[1][1] == t[2][0]:
-        if t[0][0]=="X":
+        if t[0][2]=="X":
             return 10
-        elif t[row][0]=="O":
+        elif t[0][2]=="O":
             return -10
     # no one wins : return 0
     return 0
 
+def minmax(t,j):
+    # calculer le score du je en cours:
+    score = calculerscore(t)
+    if score == 10 or score == -10:
+        return score
+   
+    # X = aximizing player: 
+    if j=='X':
+        bestScore = -1000
+        for i in range(3):
+            for j in range(3):
+                # if the place is free:
+                if (t[i][j]=='_'):
+                    t[i][j]='X'
+                    bestScore = max(bestScore, minmax(t,'O'))
+                    t[i][j]='_'
+        return bestScore
+    else: 
+        bestScore = 1000
+        for i in range(3):
+            for j in range(3):
+                # if the place is free:
+                if (t[i][j]=='_'):
+                    t[i][j]='O'
+                    bestScore = min(bestScore, minmax(t,'O'))
+                    t[i][j]='_'
+        return bestScore
+               
+
 def meilleurMouvement(t):
-    
+    bestScore = -10000
+    bestMove = (-1,-1)
     for i in range(3):
         for j in range(3):
             # if the place is free:
             if (t[i][j]=='_'):
                 t[i][j]='X'
-            nextMove = minmax(t,'O')
+            nextScore = minmax(t,'O')
             
-            if nextMove>bestMove:
+            if nextScore>bestScore:
                 bestMove = (i,j)
-                bestMove = nextMove
-                
+                bestScore = nextScore
+    print("Meilleur choix possible : ", bestMove, bestScore)
+    return bestMove
+
+
 table = [
     ['X','O','O'],
-    ['_','O','_'],
-    ['O','X','X']]
+    ['_','_','_'],
+    ['_','_','X']]
 
 
+bestMove = meilleurMouvement(table)
+print("the best move is ROW=%i COL=%i" % (bestMove[0], bestMove[1]))
